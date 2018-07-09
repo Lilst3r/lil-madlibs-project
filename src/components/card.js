041b36823d6ea409a8de82740628b2c964b80d63
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-09 13:15:02 -0600

    added Content component

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
 
 class Card extends Component {
 
@@ -39,20 +40,20 @@ class Card extends Component {
         const inputData = [
             {title: 'Color', state: this.state.color, name: 'color'},
             {title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
-            {title: 'Adjective', state: this.state.adjective, name: 'adjective'},
-            {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
-            {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
-            {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
-            {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
-            {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
-            {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
-            {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
-            {title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
-            {title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
-            {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
-            {title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
-            {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
-            {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'}
+            {title: 'Adjective One', state: this.state.adjectiveOne, name: 'adjectiveOne'},
+            {title: 'Celebrity One', state: this.state.celebOne, name: 'celebOne'},
+            {title: 'Adjective Two', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
+            {title: 'Noun One', state: this.state.nounOne, name: 'nounOne'},
+            {title: 'Number One', state: this.state.numberOne, name: 'numberOne'},
+            {title: 'Number Two', state: this.state.numberTwo, name: 'numberTwo'},
+            {title: 'Noun Two', state: this.state.nounTwo, name: 'nounTwo'},
+            {title: 'Adjective Three', state: this.state.adjectiveThree, name: 'adjectiveThree'},
+            {title: 'Celebrity Two', state: this.state.celebTwo, name: 'celebTwo'},
+            {title: 'Celebrity Three', state: this.state.celebThree, name: 'celebThree'},
+            {title: 'Adjective Four', state: this.state.adjectiveFour, name: 'adjectiveFour'},
+            {title: 'Noun Three', state: this.state.nounThree, name: 'nounThree'},
+            {title: 'Celebrity Four', state: this.state.celebFour, name: 'celebFour'},
+            {title: 'Adjective Five', state: this.state.adjectiveFive, name: 'adjectiveFive'}
         ]
 
         return (
@@ -60,8 +61,7 @@ class Card extends Component {
                 {
                     inputData.map(data => Input( (data), this.handleInputChange ))
                 }
-                
-                
+                <Content data={this.state}/>
             </div>
         )
     }
